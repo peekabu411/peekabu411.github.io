d2baafb9aedb2f81247d6f472c369ef3210945f8
Luigi Mendoza <316849128+peekabu411@users.noreply.github.com>
2026-08-19 14:09:24 +0800

    Restore exact I.9.7.3 release state

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "turntable-ios-i9717-guide-improvements";
-const ASSETS = ["./", "./index.html", "./styles.css", "./app.js?v=I.9.7.7-guide-improvements", "./manifest.webmanifest"];
+﻿const CACHE = "turntable-ios-root-v1";
+const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", event => {
